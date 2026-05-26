779fee512a2ea61bed615edf8d06de8e70611635
boolasunkanmi <olasunkanmiboluwatife110@gmail.com>
2026-05-26 09:13:21 +0100

    feat: disable X-Powered-By header to enhance security by reducing information leakage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Disable X-Powered-By header to reduce information leakage
+  poweredByHeader: false,
 };
 
 export default nextConfig;
