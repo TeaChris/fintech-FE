ebc2c61d82b1f3f9ace0304d2e566f64aa232879
boolasunkanmi <olasunkanmiboluwatife110@gmail.com>
2026-06-18 12:50:24 +0100

    feat: implement request timeout management and typed mutation hooks for the API client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components'
+
 export default async function Page() {
-      return <div></div>;
+      return (
+            <div className="max-w-full min-h-screen bg-gray-50">
+                  <h1 className="text-2xl text-rose-500 font-extrabold antialiased">
+                        Beta Payment Gateway
+                  </h1>
+
+                  <Button>Click me</Button>
+            </div>
+      )
 }
